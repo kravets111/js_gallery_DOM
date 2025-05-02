033de8c0190e8433ff44b3af93970a9175497065
Alexey Kravets <kravecoleksiy@gmail.com>
2025-05-02 22:31:05 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const imageElement = document.querySelector('.gallery');
+
+const targetImage = (e) => {
+  e.preventDefault();
+
+  const target = e.target;
+
+  if (target.tagName === 'IMG') {
+    const link = target.closest('.list-item__link');
+
+    largeImg.src = link.href;
+  }
+};
+
+imageElement.addEventListener('click', targetImage);
